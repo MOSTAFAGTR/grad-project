625b41dd5db5516a9c0494acae62ceb70a8c6ad7
MOSTAFAGTR <168591264+MOSTAFAGTR@users.noreply.github.com>
2026-03-29 03:29:50 +0200

    Penteration Testing & 1 add

--- backend/uploads/extracted_projects/0bf70b863a1b4ef2ad6131afc2536fa7/grad-project-main/challenge-redirect/run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Test script starts the app via gunicorn in setUpClass
-python test_app.py
--- backend/uploads/extracted_projects/3fb186b3b30146ac915019780733e9ee/grad-project-main/challenge-xss/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Starting Flask server..."
-python app.py &
-sleep 3
-echo "Running tests..."
-python test_app.py
